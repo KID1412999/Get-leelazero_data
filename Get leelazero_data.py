e38403913ea8c384f5a5b3bbfd91d4f707da4ab3
K.I.D <33932311+KID1412999@users.noreply.github.com>
2018-04-19 12:46:14 +0800

    Update Get leelazero_data.py

--- Get leelazero_data.py
+++ Get leelazero_data.py
@@ -1,9 +1,18 @@
 import requests
 import json
-import demjson
-js_url="http://zero.sjeng.org/data/elograph.json"
-data=requests.get(js_url)
-data=data.text
-data=json.dumps(data)
-text = demjson.decode(data)
-print(text)
+st=[]
+url='http://null:null@zero.sjeng.org/data/elograph.json'
+html=requests.get(url)
+t=html.text
+t= json.loads(t)
+for i in range(len(t)):
+	if t[i]['best']==True:
+		print(t[i])
+		st.append(str(t[i]['net'])+'>'+str(t[i]['rating'])+'>'+str(t[i]['hash']))
+print(st)
+f=open('E:\\leelazero_data.txt','w')
+f.write('训练数量'+'>'+'Elo值'+'>'+'权重名'+'\n')
+for i in range(len(st)):
+	f.write(st[i]+'\n')
+f.close()
+print('请打开E://leelazero_data.txt查看数据!')
